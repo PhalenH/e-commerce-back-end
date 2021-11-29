5ecc6e52b796d046610da0fd585ddebb28b406f5
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-11-28 22:06:27 -0500

    worked on/almost finished models, sync sequalize models in server.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  // onDelete: 'CASCADE', --should I add this?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongToMany(Tag, {
+  through: {
+    model: ProductTag,
+    // unique: false --should I add this?
+  },
+  as: 'generic_tags'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(Product, {
+  through: {
+    model: ProductTag,
+    // unique: false --should I add this?
+  },
+  as: 'generic_products'
+});
 
 module.exports = {
   Product,
