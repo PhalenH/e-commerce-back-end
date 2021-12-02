f3d82f2fa1caa5eb3a870cfd4868cb60c9e6e2bc
Phalen Hazel <88113752+PhalenH@users.noreply.github.com>
2021-12-01 22:16:58 -0500

    added to .get routes in tag-routes.js

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,11 +4,34 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: [{ Model: Product }],
+    });
+    
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ Model: Product }],
+    });
+
+    if (!tagData) {
+      res.status(404).json({ message: "No tag found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
